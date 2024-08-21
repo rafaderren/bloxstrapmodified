91635f0273b6ff7ab908d7a9092b584f274e7f9b
Crayon <160785498+CrayonEatar@users.noreply.github.com>
2024-08-21 15:35:29 +0100

    Test
    wawa

---
+++ Bloxstrap/UI/ViewModels/Settings/ThemeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+using Microsoft.Win32;
+
+using CommunityToolkit.Mvvm.Input;
+
+using Bloxstrap.Models.SettingTasks;
+
+namespace Bloxstrap.UI.ViewModels.Settings
+{
+    public class ThemeViewModel : NotifyPropertyChangedViewModel
+    {
+    }
+}
+ 
